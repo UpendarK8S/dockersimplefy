de5b4c274623a767fddc19883dbc9bb05e3c9c28
Upendar Arvapalli <119287820+UpendarK8S@users.noreply.github.com>
2022-12-07 22:02:05 +0530

    second commit

---
+++ src/main/java/dockersimplefy/Test.java
@@ -0,0 +1,10 @@
+package dockersimplefy;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Helloo finally smile baby..");
+	}
+
+}
