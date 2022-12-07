4c1ed9d8463f343925025e081073b77711821e84
Upendar Arvapalli <119287820+UpendarK8S@users.noreply.github.com>
2022-12-07 22:08:36 +0530

    3 rdline

--- src/main/java/dockersimplefy/Test3.java
+++ src/main/java/dockersimplefy/Test3.java
@@ -1,10 +1,11 @@
 package dockersimplefy;
 
-public class Test {
+public class Test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Helloo finally smile baby..");
+		System.out.println("hellow 2 sec");
+		
 	}
 
 }
